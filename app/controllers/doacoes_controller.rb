10931e82438dc17267db7097bd17c846c7cfdb15
Thiago <thiagofmares@outlook.com>
2024-12-10 23:32:10 -0300

    Adicionando os métodos put

--- app/controllers/doacoes_controller.rb
+++ app/controllers/doacoes_controller.rb
@@ -106,6 +106,39 @@ class DoacoesController < ApplicationController
     end
   end
 
+  def modifica_agendamento
+  begin
+    id = params[:id]
+    id_doacao = params[:id_doacao]
+    nova_data = params[:nova_data]
+
+    if id.blank? || id_doacao.blank? || nova_data.blank?
+      render json: { error: "campos inválidos" }, status: :bad_request
+      nil
+    end
+
+    doacao = Doacao.find_by(id: id_doacao, user_id: id)
+    doacoes = Doacoes.find(id).pluck(:donation_date)
+    gender = User.find(id).gender
+
+    doacoes.each do |data|
+      validador  = validador.new(id, nova_data, data, gender)
+
+      unless validador.confirma_doacao
+        render json: {
+          error: "Data inválida para doação"
+        }
+        return
+      end
+    end
+    doacao.update(donation_date: nova_data)
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { error: "Doação não encontrada: #{e.message}" }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+
   def deleta_agendamento
     begin
       ids = params[:ids]
